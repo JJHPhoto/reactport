c6c71ba84d2b066d2bdb5a1f1f37e3a12fe66abe
Joshua J. Haller <phone@joshuajhaller.com>
2021-05-10 16:36:50 -0700

    Worked out the header component. Need to find better image.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,38 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import Typical from "react-typical";
+import content from "../../content";
 
 function Header() {
   return (
-    <div>
-      <header>
-        <div class="p-5 text-center bg-light">
-          <h1 class="mb-3">Joshua J Haller</h1>
-          {/* <h4 class="mb-3">Needed?</h4> */}
-          <a
-            class="btn btn-primary btn-rounded mx-2"
-            href="www.joshuajhaller.com"
-            role="button"
-          >
-            Photography
-          </a>
-          <a
-            class="btn btn-primary btn-rounded mx-2"
-            href="https://jjh-webdev-portfolio.herokuapp.com"
-            role="button"
-          >
-            Web Development
-          </a>
+    <div
+      className="min-h-screen flex items-center justify-center"
+      style={{ background: "#3f2a59" }}
+    >
+      <div className="w-10/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between">
+        <div className="w-full md:w-2/5">
+          <LazyLoadImage src={content.header.img} effect="blur" />
         </div>
-      </header>
+        <div className="text-white font-dosis text-center md:text-left">
+          <h2 className="text-3xl md:text-5xl font-bold">
+            {content.header.text[0]}
+            <br />
+            {content.header.text[1]}
+          </h2>
+          <h1 className="font-bold text-2xl text-gray-500">
+            {content.header.text[2]}
+            <Typical
+              steps={["web developer.", 1000, "photographer.", 1000]}
+              loop={Infinity}
+              className="inline-block"
+            />
+          </h1>
+          <button className=" animate-float bg-pink-500 px-10 py-3 text-xl uppercase mt-10 rounded-lg">
+            {content.header.btnText}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/content/index.js
+++ src/content/index.js
@@ -1,9 +1,16 @@
+import headerImg from "./banner-fog-city.jpg";
+
 export default {
   nav: {
-    logo: "JJH",
+    logo: "JH",
     links: [
       { text: "Work", to: "mywork" },
       { text: "Contact", to: "mycontact" },
     ],
+  },
+  header: {
+    img: headerImg,
+    text: ["Hi!", "I'm Joshua Haller", "I'm a "],
+    btnText: "Learn More",
   },
 };
