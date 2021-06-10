61e5ed0c3fb4cc25ba87d6090a7909ba6a5358cd
Joshua J. Haller <phone@joshuajhaller.com>
2021-06-10 08:32:02 -0700

    Added ScrollLink package to Navbar component.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 import content from "../../content";
 
 function Navbar() {
@@ -7,18 +8,27 @@ function Navbar() {
       style={{
         background: "#3f2a59",
       }}
+      className="font-dosis fixed top-0 w-screen z-40"
     >
-      <div className="flex items-center justify-between w-10/12 mx-auto py-3 text-white font-dosis">
-        <h1 className="text-3xl font-bold">
-          {content.nav.logo}{" "}
-          <span className="w-3 h-3 bg-pink-500 inline-block rounded-full"></span>{" "}
-        </h1>
-        <div>
+      <div className="flex items-center justify-between w-10/12 mx-auto py-3">
+        <ScrollLink
+          to="header"
+          smooth={true}
+          className="text-3xl font-bold text-white flex-1 cursor-pointer"
+        >
+          <h1 className="text-3xl font-bold">
+            {content.nav.logo}{" "}
+            <span className="w-3 h-3 bg-pink-500 inline-block rounded-full"></span>{" "}
+          </h1>
+        </ScrollLink>
+        <div className="text-white text-xl">
           {content.nav.links.map((link, index) => {
             return (
-              <span key={index} className="text-xl mr-4">
-                {link.text}
-              </span>
+              <ScrollLink to={link.to} smooth={true}>
+                <span key={index} className="text-xl mr-4">
+                  {link.text}
+                </span>
+              </ScrollLink>
             );
           })}
         </div>
